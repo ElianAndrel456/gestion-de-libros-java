af170ef5a18aca0d10c15f69af860f0294dc6c7b
Elian <elian7joyo7villanueva@gmail.com>
2025-07-08 10:02:44 -0500

    Implementacion de la clase

--- src/Libro.java
+++ src/Libro.java
@@ -1,3 +1,61 @@
-public class Libro {
 
+public class Libro {
+  private String isbn;
+  private String titulo;
+  private String autor;
+  private String genero;
+  private int anioPublicacion;
+
+  public Libro(String isbn, String titulo, String autor, String genero, int anioPublicacion) {
+    this.isbn = isbn;
+    this.titulo = titulo;
+    this.autor = autor;
+    this.genero = genero;
+    this.anioPublicacion = anioPublicacion;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public String getGenero() {
+    return genero;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public int getAnioPublicacion() {
+    return anioPublicacion;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public void setGenero(String genero) {
+    this.genero = genero;
+  }
+
+  public void setAnioPublicacion(int anioPublicacion) {
+    this.anioPublicacion = anioPublicacion;
+  }
+
+  @Override
+  public String toString() {
+    return titulo + " - " + autor + " (" + anioPublicacion + ")";
+  }
 }
